9e27e621285e197b4483e62e344c74eb2b4fc665
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2023-05-21 18:24:00 -0400

    began editing basic homepage

--- pokemon-react-native/components/home/welcome/Welcome.jsx
+++ pokemon-react-native/components/home/welcome/Welcome.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from 'react'
+import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native'
+import { useRouter } from 'expo-router'
+
 
 import styles from './welcome.style'
+import {icons, SIZES, COLORS, FONTS} from '../../../constants'
 
 const Welcome = () => {
+  const router = useRouter()
   return (
     <View>
       <Text>Welcome</Text>
