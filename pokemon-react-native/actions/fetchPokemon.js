8090cb244323751ad9047aeb8b6be9100a4912a0
1ShoukR <rahminshoukoohi@gmail.com>
2023-05-28 16:24:03 -0400

    added axios command to fetchPokemon function

--- pokemon-react-native/actions/fetchPokemon.js
+++ pokemon-react-native/actions/fetchPokemon.js
@@ -1,6 +1,14 @@
-import axios from "axios"
-
+import axios from 'axios';
 
 const fetchPokeomon = () => {
-    
-}
+	axios
+		.get('https://pokeapi.co/api/v2/ability/{id or name}/')
+		.then(function (response) {
+			// handle success
+			console.log(response);
+		})
+		.catch(function (error) {
+			// handle error
+			console.log(error);
+		});
+};
