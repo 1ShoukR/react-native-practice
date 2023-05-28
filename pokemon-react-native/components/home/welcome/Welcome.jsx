ac5737f8f2a5cc1e5dc49d324b5c6166ef7af181
1ShoukR <rahminshoukoohi@gmail.com>
2023-05-28 19:12:16 -0400

    finally added background text for placeholder

--- pokemon-react-native/components/home/welcome/Welcome.jsx
+++ pokemon-react-native/components/home/welcome/Welcome.jsx
@@ -1,27 +1,24 @@
-import { useState } from 'react'
-import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native'
-import { useRouter } from 'expo-router'
+import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native';
+import { useRouter } from 'expo-router';
 
-
-import styles from './welcome.style'
-import {icons, SIZES, COLORS, FONTS} from '../../../constants'
+import styles from './welcome.style';
+import { icons, SIZES, COLORS, FONTS } from '../../../constants';
 
 const Welcome = () => {
-  const router = useRouter()
-  return (
-    <View>
-      <View styles={styles.container}>
-        <Text style={styles.userName}>Hello Rahmin</Text>
-        <Text style={styles.welcomeMessage}>Welcome to your Pokedex</Text>
-      </View>
-      <View style={styles.searchContainer}>
-        <View style={styles.searchWrapper}>
-          <TextInput style={styles.searchInput} 
-          />
-        </View>
-      </View>
-    </View>
-  )
-}
+	const router = useRouter();
+	return (
+		<View>
+			<View style={styles.container}>
+				<Text style={styles.userName}>Hello Rahmin</Text>
+				<Text style={styles.welcomeMessage}>Welcome to your Pokedex</Text>
+			</View>
+			<View style={styles.searchContainer}>
+				<View style={styles.searchWrapper}>
+					<TextInput placeholderTextColor="#000" placeholder="search for your pokemon" style={styles.searchInput} />
+				</View>
+			</View>
+		</View>
+	);
+};
 
-export default Welcome
+export default Welcome;
