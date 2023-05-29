700af9ecb21cf848e64d84c22c49e7cd61548550
1ShoukR <rahminshoukoohi@gmail.com>
2023-05-29 17:56:32 -0400

    converted to async await function

--- pokemon-react-native/actions/fetchPokemon.js
+++ pokemon-react-native/actions/fetchPokemon.js
@@ -1,24 +1,18 @@
 import axios from 'axios';
+import { useState } from 'react';
 import { Alert } from 'react-native';
 
-export const fetchPokemon = (lowercaseSearchPokemon) => {
+export const fetchPokemon = async (lowercaseSearchPokemon) => {
 	console.log('this is the searchPokemon', lowercaseSearchPokemon);
-	axios
-		.get(`https://pokeapi.co/api/v2/pokemon/${lowercaseSearchPokemon}/`)
-		.then(function (response) {
-			// handle <successwq></successwq>
-			console.log(response);
-		})
-		.catch(function (error) {
-			// handle error
-			console.log(error);
-			Alert.alert('Error', 'Pokemon not found', [
-				{
-					text: 'OK',
-					onPress: () => console.log('OK Pressed'),
-				},
-			]);
-		});
+	try {
+		const response = await axios
+			.get(`https://pokeapi.co/api/v2/pokemon/${lowercaseSearchPokemon}/`);
+		// handle success
+		console.log(response);
+		return response.data;
+	} catch (error) {
+		// handle error
+		console.log(error);
+		throw error; // Rethrow the error to be caught in the calling function
+	}
 };
-
-
