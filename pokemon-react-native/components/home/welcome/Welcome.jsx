2607079a3d298647ef875e8a888489d8c0246408
1ShoukR <rahminshoukoohi@gmail.com>
2023-05-29 17:57:12 -0400

    created a spinner for loading data:

--- pokemon-react-native/components/home/welcome/Welcome.jsx
+++ pokemon-react-native/components/home/welcome/Welcome.jsx
@@ -1,21 +1,39 @@
-import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Image, FlatList, Alert } from 'react-native';
 import { useState } from 'react';
 import { useRouter } from 'expo-router';
 import { fetchPokemon } from '../../../actions/fetchPokemon';
 
 import styles from './welcome.style';
 import { icons, SIZES, COLORS, FONTS } from '../../../constants';
+import ContentLoader from 'react-native-easy-content-loader';
 
 const Welcome = () => {
 	const router = useRouter();
-  const [searchPokemon, setSearchPokemon] = useState('');
-  console.log("searchPokemon", searchPokemon)
+	const [searchPokemon, setSearchPokemon] = useState('');
+	const [isLoading, setIsLoading] = useState(false);
 
+	const handlePokemonSearchPress = async () => {
+		const lowercaseSearchPokemon = searchPokemon.toLowerCase();
+		setIsLoading(true);
 
-	const handlePokemonSearchPress = () => {
-      const lowercaseSearchPokemon = searchPokemon.toLowerCase();
-			fetchPokemon(lowercaseSearchPokemon);
+		try {
+			console.log('Before await');
+			const data = await fetchPokemon(lowercaseSearchPokemon);
+			console.log('After await');
+			setIsLoading(false);
+			console.log('data', data);
+		} catch (error) {
+			setIsLoading(false);
+			console.log(error);
+			Alert.alert('Error', 'Pokemon not found', [
+				{
+					text: 'OK',
+					onPress: () => console.log('OK Pressed'),
+				},
+			]);
+		}
 	};
+
 	return (
 		<View>
 			<View style={styles.container}>
@@ -26,12 +44,13 @@ const Welcome = () => {
 				<View style={styles.searchWrapper}>
 					<TextInput onChangeText={setSearchPokemon} value={searchPokemon} placeholderTextColor={COLORS.gray} placeholder="search for your pokemon" style={styles.searchInput} />
 				</View>
-        <View>
-          <TouchableOpacity style={styles.searchBtn} onPress={handlePokemonSearchPress}>
-            <Image  source={icons.search} resizeMode="contain" style={styles.searchBtnImage} />
-          </TouchableOpacity>
-        </View>
+				<View>
+					<TouchableOpacity style={styles.searchBtn} onPress={handlePokemonSearchPress}>
+						<Image source={icons.search} resizeMode="contain" style={styles.searchBtnImage} />
+					</TouchableOpacity>
+				</View>
 			</View>
+			{isLoading ? <ContentLoader active avatar /> : null}
 		</View>
 	);
 };
